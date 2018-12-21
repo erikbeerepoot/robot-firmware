c7edf3b4caddc4512fb2d24528e14aff427f5f2e
Erik Beerepoot 😊 <erikbeerepoot@hush.com>
2018-12-21 08:24:49 -0500

    Add pwm hbridge based motor driver

--- Inc/motion/Motor.h
+++ Inc/motion/Motor.h
@@ -5,4 +5,39 @@
 #ifndef ROBOT_MOTOR_H
 #define ROBOT_MOTOR_H
 
+#include <stm32f446xx.h>
+#include <stm32f4xx_hal.h>
+
+class Motor {
+public:
+    Motor(GPIO_TypeDef *directionPinAPort,
+          uint16_t directionPinA,
+          GPIO_TypeDef *directionPinBPort,
+          uint16_t directionPinB,
+          TIM_HandleTypeDef *pwmTimer,
+          uint16_t pwmChannel,
+          uint32_t compareValue);
+
+    int setDuty(int duty);
+    int getDuty();
+
+    void run(bool reverse);
+    void stop();
+private:
+    uint32_t compute_duty(int duty);
+    void _a1(GPIO_PinState state);
+    void _a2(GPIO_PinState state);
+
+    int duty = 0;
+    uint32_t baseCompareValue = 0;
+
+    // Define direction pins
+    GPIO_TypeDef *a1Port;
+    GPIO_TypeDef *a2Port;
+    uint16_t a1Channel;
+    uint16_t a2Channel;
+    TIM_HandleTypeDef *pwmTimer;
+    uint16_t pwmChannel;
+};
+
 #endif //ROBOT_MOTOR_H
--- Src/motion/Motor.cpp
+++ Src/motion/Motor.cpp
@@ -1,4 +1,77 @@
 //
 // Created by Erik Beerepoot 😊 on 2018-12-19.
 //
+#include <common/Common.h>
+#include "../../Inc/motion/Motor.h"
 
+Motor::Motor(GPIO_TypeDef *directionPinAPort,
+             uint16_t directionPinA,
+             GPIO_TypeDef *directionPinBPort,
+             uint16_t directionPinB,
+             TIM_HandleTypeDef *timer,
+             uint16_t timerChannel,
+             uint32_t compareValue) {
+    a1Port = directionPinAPort;
+    a2Port = directionPinBPort;
+    a1Channel = directionPinA;
+    a2Channel = directionPinB;
+    pwmTimer = timer;
+    pwmChannel = timerChannel;
+    baseCompareValue = compareValue;
+}
+
+uint32_t Motor::compute_duty(int duty) {
+    //clamp to valid values
+    if (duty > 100) {
+        duty = 100;
+    } else if (duty < 0) {
+        duty = 0;
+    }
+    return (uint32_t)(baseCompareValue * ((float)duty/100));
+}
+
+
+int Motor::setDuty(int newDuty) {
+    if (newDuty > 100) {
+        duty = 100;
+    } else if (newDuty < 0) {
+        duty = 0;
+    } else {
+        duty = newDuty;
+    }
+
+    uint32_t period = compute_duty(duty);
+    __HAL_TIM_SET_COMPARE(pwmTimer, pwmChannel, period);
+
+    return duty;
+}
+
+int Motor::getDuty(){
+    return duty;
+}
+
+void Motor::_a1(GPIO_PinState state) {
+    //PA0
+    HAL_GPIO_WritePin(a1Port, a1Channel, state);
+}
+
+void Motor::_a2(GPIO_PinState state) {
+    //PA1
+    HAL_GPIO_WritePin(a2Port, a2Channel, state);
+}
+
+void Motor::run(bool reverse) {
+    //PA0, PA1
+    if(reverse) {
+        _a1(LOW);
+        _a2(HIGH);
+    } else {
+        _a1(HIGH);
+        _a2(LOW);
+    }
+}
+
+void Motor::stop(){
+    _a1(LOW);
+    _a2(LOW);
+}

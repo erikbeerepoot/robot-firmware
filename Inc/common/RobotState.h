8ab8148e7eb1ae29286bf5a9b03bd1cf275567f8
Erik Beerepoot 😊 <erikbeerepoot@hush.com>
2018-12-21 08:23:54 -0500

    Add model to capture robot state

--- Inc/common/RobotState.h
+++ Inc/common/RobotState.h
@@ -5,13 +5,21 @@
 #ifndef ROBOT_ROBOTSTATE_H
 #define ROBOT_ROBOTSTATE_H
 
-typedef enum RobotState {
+typedef enum MotionState {
     STRAIGHT = 0,
     LEFT,
     RIGHT,
     STOPPED,
     REVERSE,
+    CHOOSE_DIRECTION,
     FAULT
+} MotionState ;
+
+typedef struct RobotState {
+    MotionState motionState = STOPPED;
+    float usonic = 0;
+    float ir = 0;
+    bool qs18 = false;
 } RobotState;
 
 #endif //ROBOT_ROBOTSTATE_H
